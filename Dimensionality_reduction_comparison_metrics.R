bb3b5499a778ccf954b89dc6f5b191b462991631
Emi <emilie.mathian@insa-lyon.fr>
2019-06-17 17:39:51 +0200

    Fix error moran in high dimension

--- Dimensionality_reduction_comparison_metrics.R
+++ Dimensionality_reduction_comparison_metrics.R
@@ -61,8 +61,11 @@ spatial_cor_main <-function(l_coords_data , spatial_data, listK, nsim = 99){
   # Merging 
   colnames(spatial_data)[1] <- "Sample_ID"
   L_coords_data <- list()
+  Spa_data <- list() # With the same sample IDs than the ones in L_coords_data[[i]]
   for (i in 1:length(l_coords_data)){
     c_data <- l_coords_data[[i]]
+    print("Dim c data")
+    print(dim(c_data))
     colnames(c_data)[1] <- "Sample_ID"
     if (dim(c_data)[1] != dim(spatial_data)[1]){
       warning(paste("The number of rows between the coordinate data frame [", i,"] and the spatial attribute data frame differs. A inner join is going to be done.", sep=""))
@@ -74,40 +77,66 @@ spatial_cor_main <-function(l_coords_data , spatial_data, listK, nsim = 99){
     c_data <- data_m[, (dim(spatial_data)[2]+1):dim(data_m)[2]]
     c_data <- cbind(data_m[, 1], c_data)
     colnames(c_data)[1] <- "Sample_ID"
+    print("Dim c data after merging")
+    print(dim(c_data))
     L_coords_data[[i]] <- c_data
     c_spatial_data <- data_m[, 1:dim(spatial_data)[2]]
-    #print(dim(c_data))
-    #print(dim(c_spatial_data))
+    Spa_data[[i]] <- c_spatial_data
   }
-  spatial_data <- c_spatial_data
   l_coords_data <- L_coords_data
-
-  sample_ID_vect = L_coords_data[[1]]$Sample_ID
+  print('ok1')
   MI_array <- array(rep(NA, length(l_coords_data)* (dim(spatial_data)[2]-1)*length(listK)), dim=c(length(l_coords_data), (dim(spatial_data)[2]-1), length(listK)))
   MS_array <- array(rep(NA, length(l_coords_data)* (dim(spatial_data)[2]-1)*length(listK)), dim=c(length(l_coords_data), (dim(spatial_data)[2]-1), length(listK)))
-  #print(dim(MI_array ))
-  #print('ok1')
+  print('ok2')
   for (i in 1:length(l_coords_data)){
+    print('i')
+    print(i)
     c_data <- l_coords_data[[i]]
-    c_data <- as.matrix(c_data[, 2:dim(c_data)[2]])
-    print('oki')
+    
+    print('')
+    print('')
+    print('')
+    print('dim c data')
+    print(dim(c_data))
+    print(class(c_data))
+    spatial_data <- Spa_data[[i]]
+    c_sample_id <- spatial_data[ ,1]
+    print('')
+    print('')
+    print('')
+    print('dim spa data')
+    print(dim(spatial_data))
+    print('ok3')
+    if (dim(c_data)[2] == 3){
+      c_data <- as.matrix(c_data[, 2:dim(c_data)[2]])
+      print('oki')
+      print(head(c_data))
+    }
     for (c_k in 1:length(listK)){
-      # ADD an if 
-      #print("c_k")
-      #print(listK[c_k])
-      k_neigh <- knn2nb(knearneigh(c_data, k=listK[c_k], RANN=FALSE))
-      #print( k_neigh[[1]])
-      ww <- nb2listw(k_neigh, style='B')
-      #print(ww$neighbours[[1]])
-      #print('okk')
+      print('In k loop')
+      if (dim(c_data)[2] == 2){
+        print("here")
+        k_neigh <- knn2nb(knearneigh(c_data, k=listK[c_k], RANN=FALSE))
+        print(k_neigh)
+        ww <- nb2listw(k_neigh, style='B')
+        print('ok4')
+      }
       for (j in 2:dim(spatial_data)[2]){
-        MI <- moran(spatial_data[, j], ww, n=length(ww$neighbours), S0=Szero(ww))
-        #print("spatial_data[, j]")
-        #print(spatial_data[, j])
-        MS <- moran.mc(spatial_data[, j], ww, nsim=99)
-        MI_array[i,(j-1),c_k] <- MI$I
-        MS_array[i,(j-1),c_k] <- MS$p.value
-        #print('okj')
+        if (dim(c_data)[2] == 2){
+          MI <- moran(spatial_data[, j], ww, n=length(ww$neighbours), S0=Szero(ww))
+          MS <- moran.mc(spatial_data[, j], ww, nsim=99)
+          MI_array[i,(j-1),c_k] <- MI$I
+          MS_array[i,(j-1),c_k] <- MS$p.value
+          print('ok5')
+        }
+        else{
+          print(length(as.character(c_sample_id)))
+          print(length(spatial_data[, j]))
+          c_spatial_data <- data.frame("Sample_ID"= as.character(c_sample_id), "att" = spatial_data[, j] )
+          MI <- moran_index_HD(data = c_data, spatial_att = c_spatial_data, K = listK[c_k], merge = NULL)
+          MI_array[i,(j-1),c_k] <- MI
+          print('ok6')
+        }
       } 
     }
   }
@@ -131,20 +160,20 @@ moran_index_HD <- function(data, spatial_att, K, merge = NULL){
     colnames(c_data)[1] <- "Sample_ID"
     colnames(c_spatial_att)[1] <- "Sample_ID"
     data <- c_data
-    #print(dim(c_data))
+    print(dim(c_data))
     spatial_att <- c_spatial_att
    }
-  #print("dim data")
-  #print(dim(data))
-  #print("spatial att")
-  #print(dim(spatial_att))
-  #print(head(spatial_att))
-  #print("K")
-  #print(K)
+  print("dim data")
+  print(dim(data))
+  print("spatial att")
+  print(dim(spatial_att))
+  print(head(spatial_att))
+  print("K")
+  print(K)
   KNN_R  = get.knn(data[, 2:dim(data)[2]], k=K, algorithm=c( "brute"))
   KNN_R = KNN_R$nn.index
-  #print("KNN_R$nn.index")
-  #print(KNN_R)
+  print("KNN_R$nn.index")
+  print(KNN_R)
   m_neigh <- matrix(0, ncol = dim(KNN_R)[1], nrow =dim(KNN_R)[1])
   for (i in 1:dim(KNN_R)[1]){
     for (j in 1:dim(KNN_R)[2]){
@@ -152,8 +181,8 @@ moran_index_HD <- function(data, spatial_att, K, merge = NULL){
       m_neigh[i,n_index] = 1
     }
   }
-  #print('rowSums(m_neigh)')
-  #print(rowSums(m_neigh))
+  print('rowSums(m_neigh)')
+  print(rowSums(m_neigh))
   n <- length(spatial_att[, 2])
   y <- spatial_att[, 2]
   ybar <- mean(y)
@@ -172,7 +201,10 @@ moran_index_HD <- function(data, spatial_att, K, merge = NULL){
 
 spa_VISTA <- data.frame("Sample_ID" = as.character(spatial_att$Sample_ID) , "VISTA" = spatial_att$VISTA)
 moran_index_HD(data= r_meso_df, spatial_att= spa_VISTA, K = 190, merge =FALSE)
-moran_index_HD(data= r_meso_df, spatial_att= spa_VISTA, K = 190, merge =TRUE)
+moran_index_HD(data= r_meso_df, spatial_att= spa_VISTA, K =20, merge =TRUE)
+moran_index_HD(data= r_meso_df, spatial_att= spa_VISTA, K =20, merge =NULL)
+
+
 
 PCA_coords_order <- test_spa_coords[[1]]
 PCA_coords_order$Sample_ID <- as.character(PCA_coords_order$Sample_ID)
@@ -191,4 +223,4 @@ for (i in 2:dim(r_meso_df)[2]){
   r_meso_df[ ,i] <-as.numeric(r_meso_df[ ,i])
 }
 r_meso_df[,1]<- as.character(r_meso_df[,1])
-test_spa_coords <- spatial_cor_main(l_coords_data = list(PCA_coords_df), spatial_data = spatial_att[, 1:2], listK = c(20), nsim = 101 )
+MITEST  <- spatial_cor_main(l_coords_data = list(PCA_coords_df, UMAP_coords_NN230, r_meso_df), spatial_data = spatial_att, listK = c(20, 200), nsim = 101 )
